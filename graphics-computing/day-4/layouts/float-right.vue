babfac34a7576d4eab5aa1c0914b88d2dd652ee1
sininen-blue <alaanneil@gmail.com>
2025-08-04 01:42:17 +0800

    feat: updated float-right

--- graphics-computing/day-4/layouts/float-right.vue
+++ graphics-computing/day-4/layouts/float-right.vue
@@ -1,10 +1,10 @@
 <template >
-  <div class="slidev-layout grid grid-cols-1 md:grid-cols-2 gap-8 items-center h-full w-full px-8 py-12">
-    <div class="prose max-w-none">
+  <div class="slidev-layout grid grid-cols-2 gap-8 w-full px-8">
+    <div class="max-w-none">
       <slot />
     </div>
-    <div>
-      <img :src="image" class="w-full rounded-xl shadow-lg object-contain" />
+	<div class="flex items-center justify-center">
+      <img :src="image" class="w-full max-w-md rounded-xl shadow-lg object-contain" />
     </div>
   </div>
 </template>
